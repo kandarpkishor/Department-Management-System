09118e22096952db16d697664bc895fbc05b4d20
Kandarp Kishor Jha <79475866+kandarpkishor@users.noreply.github.com>
2021-02-27 15:56:22 +0530

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<?
+include('../connection/session.php');
+
+?>
+
+<html>
+	<head>
+	
+	</head>
+	<body>
+		<h1>Hi, This is index for Admin<h1>
+		<a href="../connection/logout.php">Log out</a>
+	</body>
+</html>
